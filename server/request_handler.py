cea40fedce351d2106e29b0646a0d37d96d2de44
Pamblam <adelphia@protonmail.ch>
2024-02-24 16:31:51 -0500

    Added symptom match

--- server/request_handler.py
+++ server/request_handler.py
@@ -52,30 +52,80 @@ if(action == "search_symptoms"):
 
     myresult = mycursor.fetchall()
 
-
-
-
-
-
-
-
-
     response['response'] = myresult
 
-elif(action == "add"):
-    num1 = float(data['num1'])
-    num2 = float(data['num1'])
-    ans = num1+num2;
-    response['response'] = str(num1)+" + "+str(num2)+" = "+str(ans)
+elif(action == "symptom_match"):
+    ids = data["ids"]
+    id_list = ids.split(",")
+    matches = []
+    if "limit" in data:
+        limit = data["limit"]
+    else:
+        limit = False
 
-elif(action == "subtract"):
-    response['response'] = "i dunno"
+    # Searching for diseases 
+    for id in id_list:
+        mycursor = mydb.cursor()
+        mycursor.execute("SELECT cause_item_id FROM diseases.cause_effect where effect_item_id = %s", [id])
+        myresult = mycursor.fetchall()
+        for item in myresult:
+            cause_id = item[0]
+            if cause_id not in matches:
+                matches.append(cause_id)
 
-elif(action == "divide"):
-    response['response'] = "i dunno"
 
-elif(action == "multiply"):
-    response['response'] = "i dunno"
+
+    # Looking for other symptoms of diseases that include initial symptom list
+    causes_list = []
+    for disease in matches:
+
+        symptoms_found = {}
+        for id in id_list:
+            id = int(id)
+            symptoms_found[id] = False
+
+
+        mycursor = mydb.cursor()
+        mycursor.execute("SELECT i.name, c.effect_item_id FROM diseases.cause_effect as c left join items as i on c.effect_item_id = i.id where cause_item_id = %s", [disease])
+        myresult = mycursor.fetchall()
+
+        # looping thru symptoms of, just to see if all items are present 
+        for result in myresult:
+            id = result[1]
+            id = int(id)
+            # if id in symptoms_found:
+            if id in symptoms_found.keys():
+                symptoms_found[id] = True
+
+        
+
+        # if all items are present
+        all_exist = True
+        for key in symptoms_found:
+            if symptoms_found[key]== False:
+                all_exist = False
+
+        if all_exist:
+            for result in myresult:
+                # does the symptom exist in the list
+                exists = False
+                # {symptom:"name", id:0, match_count:1}
+                for cause in causes_list:
+                    if cause["id"] == result[1]:
+                        exists = True
+                        cause["match_count"] += 1
+                if exists == False:
+                    causes_list.append({"symptom": result[0], "match_count": 1, "id": result[1]})
+        
+        
+
+    sorted_causes_list = sorted(causes_list, key = lambda i: i["match_count"], reverse=True) 
+    if limit: 
+        response['response'] = sorted_causes_list[:int(limit)]
+    else:
+        response['response'] = sorted_causes_list
+    # print(matches)
+
 
 else:
     err('Invalid action')
