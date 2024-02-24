c85c1b40399730edfe3d4270d5b46fe0c364aed4
Pamblam <adelphia@protonmail.ch>
2024-02-24 13:03:44 -0500

    saving

--- client/public/api/index.php
+++ client/public/api/index.php
@@ -1,1 +1,31 @@
-<?php echo "test";
+<?php
+
+define("APP_ROOT", realpath(dirname(dirname(dirname(dirname(__FILE__))))));
+require APP_ROOT."/server/includes/runcmd.php";
+define('PYTHON_INTERPRETTER', 'python3');
+define('REQUEST_HANDLER',  APP_ROOT."/server/request_handler.py");
+
+if(isset($_REQUEST)){
+    $root = "python3 ~/htdocs/python-starter/request_handler/index.py --name=pam";
+    $cmd = PYTHON_INTERPRETTER." ".REQUEST_HANDLER." --json=".escapeshellarg(json_encode($_REQUEST));
+
+    header('Content-Type: application/json');
+    try{
+        $res = runcmd($cmd);
+        if(!empty($res->stderr)){
+			echo json_encode([
+				"message" => $res->stderr,
+				"response" => [],
+				"success" => false
+			]);
+        } 
+        echo $res->stdout;
+    }catch(Exception $e){
+        echo json_encode([
+            "message" => $e->message,
+            "response" => [],
+            "success" => false
+        ]);
+        exit;
+    }
+}
